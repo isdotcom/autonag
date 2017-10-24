1522b5fafb6ff241efac07bfbbfb196d598deaa0
Bart Moorman <bmoorman@insidesales.com>
2017-10-24 13:54:41 -0600

    Working versions to add and remove hosts automagically

---
+++ web/checkDeletedInstances.php
@@ -0,0 +1,85 @@
+#!/usr/bin/php
+<?php
+class ec2Tracker {
+    private $settings = array();
+    private $params = array();
+    private $trackInstances = array();
+    private $foundInstances = array();
+
+    public function __construct($type) {
+        require('aws-sdk/aws-autoloader.php');
+        $this->settings = parse_ini_file('settings.ini', true);
+        if (!is_writable($this->settings['paths']['hostConfigDir'])) {
+            echo "{$this->settings['paths']['hostConfigDir']} is not writable." . PHP_EOL;
+            exit(1);
+        } elseif (!is_writable($this->settings['paths']['hostTrackDir'])) {
+            echo "{$this->settings['paths']['hostTrackDir']} is not writable." . PHP_EOL;
+            exit(1);
+        } elseif (!is_writable($this->settings['logs']['change'])) {
+            echo "{$this->settings['logs']['change']} is not writable." . PHP_EOL;
+            exit(1);
+        }
+        $this->params['type'] = $type;
+        $this->trackInstances = file("{$this->settings['paths']['hostTrackDir']}/{$this->params['type']}.inf", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    }
+
+    private function getCredentials() {
+        $provider = new Aws\Credentials\CredentialProvider;
+	$credentials = $provider->defaultProvider();
+        return $credentials;
+    }
+
+    private function createClient($region) {
+	$credentials = $this->getCredentials();
+        $client = new Aws\Ec2\Ec2Client([
+            'region' => $region,
+            'credentials' => $credentials,
+            'version' => 'latest'
+        ]);
+        return $client;
+    }
+
+    public function getAllInstances($region) {
+        $client = $this->createClient($region);
+        $result = $client->describeInstanceStatus([
+            'IncludeAllInstances' => true
+        ]);
+        $instances = $result['InstanceStatuses'];
+        foreach ($instances as $instance) {
+            $this->foundInstances[] = $instance['InstanceId'];
+        }
+    }
+
+    private function logChange($name, $instanceId) {
+        $date = date('r');
+        $msg = <<<EOM
+-=-=-=-=-=-
+{$date}
+-=-=-=-=-=-
+Host Removed:
+    host_name -> {$name}
+    notes -> {$instanceId}
+EOM;
+        file_put_contents($this->settings['logs']['change'], $msg . PHP_EOL, FILE_APPEND);
+    }
+
+    private function unmonitorInstance($name, $instanceId) {
+        rename("{$this->settings['paths']['hostConfigDir']}/{$name}.cfg", "{$this->settings['paths']['hostConfigDir']}/{$name}.cfg.old");
+        exec("sed -i.bak '/{$instanceId}/d' {$this->settings['paths']['hostTrackDir']}/{$this->params['type']}.inf");
+        $this->logChange($name, $instanceId);
+    }
+
+    public function findDeletedInstances() {
+        foreach ($this->trackInstances as $trackInstance) {
+            $instance = explode(':', $trackInstance);
+            if (!in_array($instance[1], $this->foundInstances)) {
+                $this->unmonitorInstance($instance[0], $instance[1]);
+            }
+        }
+    }
+}
+
+$tracker = new ec2Tracker('ecs-pod');
+$tracker->getAllInstances('us-west-2');
+$tracker->findDeletedInstances();
+?>
---
+++ web/hostConfigBuilder.php
@@ -0,0 +1,90 @@
+<?php
+class hostConfigBuilder {
+    private $settings = array();
+    private $params = array();
+
+    public function __construct() {
+        $this->settings = parse_ini_file('settings.ini', true);
+        if (!is_writable($this->settings['paths']['hostConfigDir'])) {
+            $this->badRequest(array('error' => 'Not writable', 'path' => $this->settings['paths']['hostConfigDir']));
+        } elseif (!is_writable($this->settings['paths']['hostTrackDir'])) {
+            $this->badRequest(array('error' => 'Not writable', 'path' => $this->settings['paths']['hostTrackDir']));
+        } elseif (!is_writable($this->settings['logs']['change'])) {
+            $this->badRequest(array('error' => 'Not writable', 'path' => $this->settings['logs']['change']));
+        }
+        // Make sure we have the required arguments (can come from _GET, _POST, or sometimes _COOKIES)
+        foreach ($this->settings['args']['required'] as $arg) {
+            if (isset($_REQUEST[$arg])) {
+                $this->params[$arg] = $_REQUEST[$arg];
+            } else {
+                // Populate all of the required args with **MISSING**
+                $required = array_fill_keys($this->settings['args']['required'], '**MISSING**');
+                // Overwrite all of the **MISSING** args with values we actually have
+                // This lets the user know what we receive and what we didn't
+                $this->badRequest(array('error' => 'Missing argument(s)', 'args' => array_merge($required, $_REQUEST)));
+            }
+        }
+    }
+
+    // This handles notifying the user if something went wrong
+    private function badRequest($args) {
+        header('HTTP/1.0 400 Bad Request');
+        print_r($args);
+        exit;
+    }
+
+    // Pull the template from templateURL (github?) if we can
+    private function downloadTemplate() {
+        $merge = array('%VERSION%' => $this->params['version'], '%TYPE%' => $this->params['type']);
+        $templateUrl = strtr($this->settings['paths']['templateUrl'], $merge);
+        $result = @file_get_contents($templateUrl);
+        if ($result != false) {
+            return $result;
+        } else {
+            $this->badRequest(array('error' => 'Unable to download template'));
+        }
+    }
+
+    // Merge our args with the template to create a useable config
+    private function mergeTemplate() {
+        $template = $this->downloadTemplate();
+        $merge = array('%HOSTNAME%' => $this->params['name'], '%IP%' => $this->params['ip'], '%NOTE%' => $this->params['instanceId'], '%HOSTGROUP%' => $this->params['type']);
+        return strtr($template, $merge);
+    }
+
+    private function logChange() {
+        $date = date('r');
+        $msg = <<<EOM
+-=-=-=-=-=-
+{$date}
+-=-=-=-=-=-
+Host Added:
+    version -> {$this->params['version']}
+    host_name -> {$this->params['name']}
+    address -> {$this->params['ip']}
+    hostgroups -> {$this->params['name']}
+    notes -> {$this->params['instanceId']}
+EOM;
+        file_put_contents($this->settings['logs']['change'], $msg . PHP_EOL, FILE_APPEND);
+    }
+
+    // Write the config to hostConfigDir
+    // Save the name:instanceId pair in hostTrackDir so we can track this later
+    public function writeConfig() {
+        $mergedTemplate = $this->mergeTemplate();
+        file_put_contents("{$this->settings['paths']['hostConfigDir']}/{$this->params['name']}.cfg", $mergedTemplate);
+        file_put_contents("{$this->settings['paths']['hostTrackDir']}/{$this->params['type']}.inf", "{$this->params['name']}:{$this->params['instanceId']}" . PHP_EOL, FILE_APPEND);
+        $this->logChange();
+    }
+
+    // Let Nagios know there's a new config
+    public function reloadNagios() {
+        exec('service nagios3 reload');
+        echo "The host is now being monitored" . PHP_EOL;
+    }
+}
+
+$hostConfig = new hostConfigBuilder();
+$hostConfig->writeConfig();
+$hostConfig->reloadNagios();
+?>
