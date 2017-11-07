5bcc4c2d8b554b211c9ba75b306b1171a727c030
Bart Moorman <bmoorman@insidesales.com>
2017-11-07 14:53:07 -0700

    Add usage and ability to list instances and metrics.

--- web/check_cloudwatch.php
+++ web/check_cloudwatch.php
@@ -2,12 +2,14 @@
 <?php
 require('aws-sdk/aws-autoloader.php');
 
-$shortOpts = 'p:r:i:C:L:D:S:w:c:';
-$longOpts = array('profile:', 'region:', 'instanceId:', 'ec2Metric:', 'elbMetric:', 'rdsMetric:', 'stat:', 'warning:', 'critical:');
+$shortOpts = 'p:r:i:CLDm:w:c:l';
+$longOpts = array('profile:', 'region:', 'instanceId:', 'ec2Metric', 'elbMetric', 'rdsMetric', 'metric:', 'warning:', 'critical:', 'list');
 $options = getopt($shortOpts, $longOpts);
 
-$required = array('profile', 'region', 'namespace', 'dimension', 'instance', 'metric', 'stat', 'warning', 'critical');
+$required = array('profile', 'region', 'instance', 'namespace', 'dimension', 'metric', 'warning', 'critical');
 $opts = array_fill_keys($required, null);
+
+$list = false;
 
 foreach ($options as $key => $value) {
 	switch ($key) {
@@ -27,24 +29,20 @@ foreach ($options as $key => $value) {
 		case 'ec2Metric':
 			$opts['namespace'] = 'AWS/EC2';
 			$opts['dimension'] = 'InstanceId';
-			$opts['metric'] = $value;
 			break;
 		case 'L':
 		case 'elbMetric':
 			$opts['namespace'] = 'AWS/ELB';
 			$opts['dimension'] = 'LoadBalancerName';
-			$opts['metric'] = $value;
 			break;
 		case 'D':
 		case 'rdsMetric':
 			$opts['namespace'] = 'AWS/RDS';
 			$opts['dimension'] = 'DBInstanceIdentifier';
-			$opts['metric'] = $value;
 			break;
-		case 'S':
-		case 'stat':
-			// Not implemented
-			$opts['stat'] = $value;
+		case 'm':
+		case 'metric':
+			$opts['metric'] = $value;
 			break;
 		case 'w':
 		case 'warning':
@@ -54,7 +52,26 @@ foreach ($options as $key => $value) {
 		case 'critical':
 			$opts['critical'] = $value;
 			break;
+		case 'l':
+		case 'list':
+			$list = true;
+			break;
 	}
+}
+
+function usage() {
+	echo 'Options:' . PHP_EOL;
+	echo ' -p, --profile <value>     Credential profile to use. default: default' . PHP_EOL;
+	echo ' -r, --region <value>      Region. Pulled from the credential profile unless provided. example: us-east-1' . PHP_EOL;
+	echo ' -i, --instanceId <value>  InstanceID, LoadBalancerName, or DBInstanceIdentifier to check.' . PHP_EOL;
+	echo ' -C, --ec2Metric           Use when checking EC2.' . PHP_EOL;
+	echo ' -L, --elbMetric           Use when checking ELB.' . PHP_EOL;
+	echo ' -D, --rdsMetric           Use when checking RDS.' . PHP_EOL;
+	echo ' -m, --metric <value>      The CloudWatch metric to monitor.' . PHP_EOL;
+	echo ' -w, --warning <value>     The warning threshold for this metric.' . PHP_EOL;
+	echo ' -c, --critical <value>    The critical threshold for this metric.' . PHP_EOL;
+	echo ' -l, --list                Use to list available instances or metrics to monitor.' . PHP_EOL;
+	exit(3);
 }
 
 if ($opts['profile'] && $opts['region']) {
@@ -64,19 +81,73 @@ if ($opts['profile'] && $opts['region']) {
 } elseif ($opts['region']) {
 	$clientOpts = array('profile' => 'default', 'region' => $opts['region'], 'version' => 'latest');
 } else {
-	echo 'Must specify profile, region, or both!' . PHP_EOL;
-	exit(3);
+	echo 'Must specify profile (-p), region (-r), or both!' . PHP_EOL;
+	usage();
 }
 
 if (!$opts['instance']) {
-	echo 'Must specify an instanceId!' . PHP_EOL;
-	exit(3);
+	if ($list && $opts['namespace'] && $opts['dimension']) {
+		switch ($opts['namespace']) {
+			case 'AWS/EC2':
+				$listclient = new Aws\Ec2\Ec2Client($clientOpts);
+				$result = $listclient->describeInstances();
+				printf('%-20s %-60s %-14s %-16s' . PHP_EOL, 'InstanceId', 'InstanceName', 'InstanceState', 'PrivateIpAddress');
+				foreach ($result['Reservations'] as $reservation) {
+					foreach ($reservation['Instances'] as $instance) {
+						foreach ($instance['Tags'] as $tag) {
+							if ($tag['Key'] == 'Name') {
+								$instanceName = $tag['Value'];
+							}
+						}
+						$instanceId = !empty($instance['InstanceId']) ? $instance['InstanceId'] : null;
+						$instanceState = !empty($instance['State']['Name']) ? $instance['State']['Name'] : null;
+						$privateIpAddress = !empty($instance['PrivateIpAddress']) ? $instance['PrivateIpAddress'] : null;
+						printf('%-20s %-60s %-14s %-16s' . PHP_EOL, $instanceId, $instanceName, $instanceState, $privateIpAddress);
+					}
+				}
+				break;
+			case 'AWS/ELB':
+				echo 'Not implemented!' . PHP_EOL;
+				break;
+			case 'AWS/RDS':
+				$listclient = new Aws\Rds\RdsClient($clientOpts);
+				$result = $listclient->describeDBInstances();
+				printf('%-60s %-60s %-16s' . PHP_EOL, 'DBInstanceIdentifier', 'DBClusterIdentifier', 'DBInstanceState');
+				foreach ($result['DBInstances'] as $dbinstance) {
+					$dBInstanceIdentifier = !empty($dbinstance['DBInstanceIdentifier']) ? $dbinstance['DBInstanceIdentifier'] : null;
+					$dBClusterIdentifier = !empty($dbinstance['DBClusterIdentifier']) ? $dbinstance['DBClusterIdentifier'] : null;
+					$dBInstanceStatus = !empty($dbinstance['DBInstanceStatus']) ? $dbinstance['DBInstanceStatus'] : null;
+					printf('%-60s %-60s %-16s' . PHP_EOL, $dBInstanceIdentifier, $dBClusterIdentifier, $dBInstanceStatus);
+				}
+				break;
+		}
+		exit(3);
+	} else {
+		echo 'Must specify an instanceId (-i)! Add (-C, -L, -D) and (-l) to show which ones are available.' . PHP_EOL;
+		usage();
+	}
+} elseif (!$opts['namespace'] || !$opts['dimension']) {
+	echo 'Must specify an instance type (-C, -L, -D)!' . PHP_EOL;
+	usage();
 } elseif (!$opts['metric']) {
-	echo 'Must specify a metric!' . PHP_EOL;
-	exit(3);
+	if ($list) {
+		$listclient = new Aws\CloudWatch\CloudWatchClient($clientOpts);
+		$dimensions = array('Name' => $opts['dimension'], 'Value' => $opts['instance']);
+		$result = $listclient->listMetrics([
+			'Dimensions' => [$dimensions],
+			'Namespace' => $opts['namespace']
+		]);
+		foreach ($result['Metrics'] as $metric) {
+			echo $metric['MetricName'] . PHP_EOL;
+		}
+		exit(3);
+	} else {
+		echo 'Must specify a metric (-m)! Add (-l) to show which ones are available.' . PHP_EOL;
+		usage();
+	}
 } elseif (!$opts['warning'] || !$opts['critical']) {
-	echo 'Must specify both warning and critical values!' . PHP_EOL;
-	exit(3);
+	echo 'Must specify both warning (-w) and critical (-c) values!' . PHP_EOL;
+	usage();
 }
 
 $client = new Aws\CloudWatch\CloudWatchClient($clientOpts);
